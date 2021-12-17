eb1c49f57c1a99d542be0d4b3ebe8433660a9032
Jake Macdonald <jakemac@google.com>
2021-12-17 09:12:58 -0800

    day 3 complete

---
+++ lib/day_3.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+final day3Values = [
+  for (var line in lines)
+    if (line.isNotEmpty) int.parse(line, radix: 2),
+];
+
+final lines = File('lib/day_3_input.txt').readAsLinesSync();
+
+int day3Part1() {
+  var length = lines.first.length;
+
+  var gammaRate = 0;
+  var epsilonRate = 0;
+  for (var i = 0; i < length; i++) {
+    var gammaVal = mostCommon(day3Values, length - i - 1);
+    gammaRate |= gammaVal << (length - i - 1);
+    epsilonRate << 1;
+    var epsilonVal = gammaVal ^ 1;
+    epsilonRate |= epsilonVal << (length - i - 1);
+  }
+  return gammaRate * epsilonRate;
+}
+
+int day3Part2() {
+  var length = lines.first.length;
+  var oxygenRating = 0;
+  var co2ScrubberRating = 0;
+  var oxygenMatches = day3Values;
+  for (var i = length - 1; i >= 0; i--) {
+    var oxygenBit = mostCommon(oxygenMatches, i);
+    oxygenMatches = [
+      for (var item in oxygenMatches)
+        if (readBit(item, i) == oxygenBit) item,
+    ];
+    if (oxygenMatches.length == 1) {
+      oxygenRating = oxygenMatches.first;
+      break;
+    }
+  }
+  var co2Matches = day3Values;
+  for (var i = length - 1; i >= 0; i--) {
+    var co2Bit = mostCommon(co2Matches, i) ^ 0x1;
+    co2Matches = [
+      for (var item in co2Matches)
+        if (readBit(item, i) == co2Bit) item,
+    ];
+    if (co2Matches.length == 1) {
+      co2ScrubberRating = co2Matches.first;
+      break;
+    }
+  }
+  return oxygenRating * co2ScrubberRating;
+}
+
+/// The most common bit a [position] of [day3Values], counting from
+/// right to left, zero indexed.
+int mostCommon(List<int> values, int position) {
+  var ones = 0;
+  for (var num in values) {
+    ones += readBit(num, position);
+  }
+  return ones >= values.length / 2 ? 1 : 0;
+}
+
+/// Returns the bit at [position] from [original], counting from
+/// right to left, zero indexed.
+int readBit(int original, int position) {
+  return (original & (0x1 << position)) >> position;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'day_1.dart';
 import 'day_2.dart';
+import 'day_3.dart';
 
 void main() async {
   print('Day 1: Part 1: ${day1Part1()} - Part 2: ${day1Part2()}');
   print('Day 2: Part 1: ${day2Part1()} - Part 2: ${day2Part2()}');
+  print('Day 3: Part 1: ${day3Part1()} - Part 2: ${day3Part2()}');
 }
