8bda34594b94ff680e1145f8a19ef758422238d7
Jake Macdonald <jakemac@google.com>
2024-11-18 20:37:57 +0000

    day 6

---
+++ 2023/lib/day_6.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+final input = File('lib/day_6.input.txt').readAsLinesSync();
+
+typedef Range = (int, int);
+
+extension on Range {
+  int get start => $1;
+  int get end => $2;
+}
+
+Range solve(Range possiblities, int time, int distance) {
+  int? start;
+  int? end;
+  for (var i = possiblities.start; i <= possiblities.end; i++) {
+    var traveled = i * (time - i);
+    if (traveled > distance) {
+      start ??= i;
+    } else if (start != null) {
+      end = i - 1;
+      break;
+    }
+  }
+  return (start!, end!);
+}
+
+void main() {
+  // Part 1
+  {
+    final times = input[0].readNumbers('Time:');
+    final distances = input[1].readNumbers('Distance:');
+    var answer = 1;
+    for (var i = 0; i < times.length; i++) {
+      var possiblities = solve((0, times[i]), times[i], distances[i]);
+      answer *= possiblities.end - possiblities.start + 1;
+    }
+
+    print('part 1: $answer');
+  }
+
+  // Part 2
+  {
+    var time =
+        int.parse(input[0].substring('Time:'.length).replaceAll(' ', ''));
+    var distance =
+        int.parse(input[1].substring('Distance:'.length).replaceAll(' ', ''));
+    var possiblities = solve((0, time), time, distance);
+    print('part 2: ${possiblities.end - possiblities.start + 1} $possiblities');
+  }
+}
+
+extension on String {
+  List<int> readNumbers([String prefix = '']) => [
+        for (var seed
+            in substring(prefix.length).split(' ')
+              ..removeWhere((s) => s.isEmpty))
+          int.parse(seed),
+      ];
+}
